91989c13c5e89d41ea0b32ed8d307c1e1556d650
Brendan Dagys <brendandagys@gmail.com>
2023-05-17 21:25:50 -0400

    Add more utility functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
+use serde_json::Value;
+use serde_wasm_bindgen::{from_value, to_value};
+use std::collections::HashMap;
+use std::collections::HashSet;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsValue;
 
 // Called when the wasm module is instantiated
 #[wasm_bindgen(start)]
@@ -19,16 +24,72 @@ pub fn main() -> Result<(), JsValue> {
 }
 
 #[wasm_bindgen]
-pub fn add(a: u32, b: u32) -> u32 {
-    a + b
-}
-
-#[wasm_bindgen]
 extern "C" {
-    fn alert(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hi there, {}!", name));
+pub fn keys(obj: JsValue) -> JsValue {
+    let serde_value = from_value(obj).unwrap();
+    let map: HashMap<String, Value> = serde_json::from_value(serde_value).unwrap();
+    let result: Vec<&String> = map.keys().collect();
+    return to_value(&result).unwrap();
+}
+
+#[wasm_bindgen]
+pub fn compact_array(arr: Box<[JsValue]>) -> Box<[JsValue]> {
+    (*arr)
+        .iter()
+        .filter(|&val| !(*val == JsValue::NULL || *val == JsValue::UNDEFINED))
+        .cloned()
+        .collect::<Box<[JsValue]>>()
+}
+
+#[wasm_bindgen]
+pub fn distinct(arr: Box<[JsValue]>) -> Box<[JsValue]> {
+    log("Hello, `distinct` is running...");
+    let mut result = Vec::new();
+
+    for val in arr.iter() {
+        log(&format!("{:?}", val));
+        if !result.contains(val) {
+            result.push(val.clone())
+        }
+    }
+
+    result.into_boxed_slice()
+}
+
+#[wasm_bindgen]
+pub fn int_val(val: JsValue) -> JsValue {
+    if val.is_string() {
+        log(&val.as_string().unwrap());
+        match val.as_string().unwrap().parse::<i32>() {
+            Ok(res) => return JsValue::from(res),
+            Err(_) => return JsValue::NULL,
+        }
+    }
+
+    if let Some(parsed) = val.as_f64() {
+        JsValue::from(parsed as i32)
+    } else {
+        JsValue::NULL
+    }
+}
+
+#[wasm_bindgen]
+pub fn float_val(val: JsValue) -> JsValue {
+    if val.is_string() {
+        match val.as_string().unwrap().parse::<f64>() {
+            Ok(res) => return res.into(),
+            Err(_) => return JsValue::NULL,
+        }
+    }
+
+    if let Some(parsed) = val.as_f64() {
+        JsValue::from(parsed)
+    } else {
+        JsValue::NULL
+    }
 }
